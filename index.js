3cfe60e1dd1ed06312086a7198b38075c1208121
obregonfranco <123923866+obregonfranco@users.noreply.github.com>
2023-02-07 01:10:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-document.getElementById('boton').onclick = function() {
-    console.log("Capturamos el evento click")
-    document.getElementById("prueba").innerHTML = "Estamos probando nuestro primer evento en JS"
-    document.body.style.backgroundColor = 'red'
+document.getElementById('info').onclick = function() {
+    document.getElementById("texto").innerHTML = "Fecha de nacimiento: 16/11/1999 <br>  <br> Ciudad: Mataderos, Ciudad Autónoma de Buenos Aires <br>  <br> Disponibilidad horaria: Full Time"
 }
-
-
-/**
-
-document.addEventListener('click', function(){
-    console.log("Hola mundo desde Event Listener")
-    document.getElementById("prueba").innerHTML = "Estamos probando nuestro primer evento en JS"
-})
-
-
-document.getElementById('boton').addEventListener('click', function(){
-    console.log("Hola mundo desde Event Listener")
-    document.getElementById("prueba").innerHTML = "Estamos probando nuestro primer evento en JS"
-})
-
-
-document.getElementById('boton').onclick = function() {
-    document.body.style.backgroundColor = 'red'
+document.getElementById('objetivo').onclick = function() {
+    document.getElementById("texto").innerHTML = "Actualmente estoy en búsqueda de un sitio donde poder desarrollarme profesionalmente, siento que podría integrarme perfectamente en un grupo de trabajo, donde además podría aportar mis conocimientos y seguir creciendo. Me considero una persona muy responsable y me involucro en los proyectos para dar lo mejor de mí. Me gusta estar informado y seguir capacitándome con nuevas herramientas."
 }
-
-
-document.getElementById('boton').onclick = function() {
-    document.getElementById('prueba').style.display = 'none'
+document.getElementById('educacion').onclick = function() {
+    document.getElementById("texto").innerHTML = "En curso <br> Universidad de Buenos Aires - FADU <br> Diseño Gráfico <br>  <br> 2013 - 2018 <br> Escuela Técnica N° 29 &quot;Reconquista de Buenos Aires&quot; <br> Técnico en Computación <br>  <br>  <br> Cursos <br>  <br> Marketing Digital <br> Crehana, 2020. <br> Curso de introduccion al marketing digital. <br>  <br> Desarrollador Front End Jr. <br> Acámica - Aprende Programando, 2017. <br> Curso de Desarrollo Web."
 }
-
-*/
+document.getElementById('experiencia').onclick = function() {
+    document.getElementById("texto").innerHTML = "Operador Telefónico <br> CAT Technologies, May 2022 - Nov 2022 <br> Atención telefónica, soporte técnico y solución de problemas a usuarios. <br>  <br> Diseñador Gráfico <br> BaseFirma - Global, 2021 - 2022 <br> Me desempeñe realizando tareas de diseño gráfico dentro del área de branding para todos los países donde tiene presencia la empresa. <br>  <br> Diseñador Gráfico <br> Freelance, 2018 - 2021 <br> Realización de logos, flyers, diseños para indumentaria, etc. Trabajando con herramientas como Adobe Illustrator para vectores y Adobe Photoshop. Con conocimientos en marketing digital. <br>  <br> Soporte Técnico <br> Ministerio de Desarrollo Urbano y Transporte, Jun 2018 - Dic 2018 <br> A modo de pasantía realizando tareas tales como solución de problemas de hardware y software, revisióny reparación de equipos, armado de pc, configuración de sistemas operativos y asistencia a usuarios."
+}
+document.getElementById('habilidades').onclick = function() {
+    document.getElementById("texto").innerHTML = "Marketing Digital <br>  <br> Diseño Gráfico <br>  <br> Conocimientos de Hardware"
+}
