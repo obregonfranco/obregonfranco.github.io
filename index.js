9b6fc23ca65fd7207beb3e195b65ec69e7c41c47
obregonfranco <123923866+obregonfranco@users.noreply.github.com>
2023-02-08 01:07:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,20 @@ document.getElementById('habilidades').onclick = function() {
 }
 
 
-$(document).ready(function(){ irArriba(); });
+$(document).ready(function(){
 
-function irArriba(){
-  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },300); });
-  $(window).scroll(function(){
-    if($(this).scrollTop() > 20){ $('.ir-arriba').slideDown(300); }else{ $('.ir-arriba').slideUp(300); }
-  });
-}
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
+});
